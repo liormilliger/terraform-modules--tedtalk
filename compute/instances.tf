d3846d9691700bcdf18419c6f445bcee8863d789
liorm <lior@email.com>
2023-09-22 11:24:17 +0300

    changing userdata file

--- compute/instances.tf
+++ compute/instances.tf
@@ -11,12 +11,12 @@ resource "aws_instance" "liorm-TED" {
   count = terraform.workspace == "default" ? 1 : 3
   ami                    = var.AMI
   instance_type          = var.TYPE
-  # key_name               = aws_key_pair.liorm-tf-key_rsa.id
+  key_name               = aws_key_pair.liorm-tf-key_rsa.id
   subnet_id = var.subnets_1a
   availability_zone      = "us-east-1a"
   vpc_security_group_ids = [var.security_group]
 
-  user_data = file("../../docker-compose.yaml")
+  user_data = file("./compute/userdata.sh")
 
   tags = {
     Name = "TED-test-${var.NAME_TAG}"
--- compute/userdata.sh
+++ compute/userdata.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-sudo apt update
-sudo apt install docker -y
-sudo apt install docker.io -y
-sudo apt update
-docker pull adongy/hostname-docker
-docker run -d --name app -p 80:3000 adongy/hostname-docker .
+
+# Install Docker and Docker Compose
+apt-get update -y
+apt-get install -y docker.io
+# curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# chmod +x /usr/local/bin/docker-compose
+
+# Fetch docker-compose.yaml from S3 bucket and perform docker-compose up
+aws s3 cp s3://liorm-ted/userdata/docker-compose.yaml ./
+# cd /path/to/destination/
+docker compose up --build
