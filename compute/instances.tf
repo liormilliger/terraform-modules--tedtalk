704d38ed1455426a53de825b9f882f2405960766
liorm <lior@email.com>
2023-09-22 08:35:10 +0300

    key-pair resource from within aws

--- compute/instances.tf
+++ compute/instances.tf
@@ -1,6 +1,10 @@
+# resource "aws_key_pair" "liorm-tf-key_rsa" {
+#   key_name   = "liorm-tf-key_rsa"
+#   public_key = file("~/.ssh/liorm-tf-key_rsa.pub")
+# }
 resource "aws_key_pair" "liorm-tf-key_rsa" {
   key_name   = "liorm-tf-key_rsa"
-  public_key = file("~/.ssh/liorm-tf-key_rsa.pub")
+  public_key = var.public_key_content
 }
 
 resource "aws_instance" "liorm-TED" {
