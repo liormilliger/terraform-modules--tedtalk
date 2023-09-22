89a5641d85aa8e3f7e6a1055bbacc7c732d83585
liorm <lior@email.com>
2023-09-22 11:37:16 +0300

    hashing key_name

--- compute/instances.tf
+++ compute/instances.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "liorm-TED" {
   count = terraform.workspace == "default" ? 1 : 3
   ami                    = var.AMI
   instance_type          = var.TYPE
-  key_name               = aws_key_pair.liorm-tf-key_rsa.id
+  # key_name               = aws_key_pair.liorm-tf-key_rsa.id
   subnet_id = var.subnets_1a
   availability_zone      = "us-east-1a"
   vpc_security_group_ids = [var.security_group]
