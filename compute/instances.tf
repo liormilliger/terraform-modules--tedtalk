d1c89c0f603287478e046ec2393a4a3b019715a1
liorm <lior@email.com>
2023-09-22 08:53:49 +0300

    disabling key pair on ec2

--- compute/instances.tf
+++ compute/instances.tf
@@ -2,16 +2,16 @@
 #   key_name   = "liorm-tf-key_rsa"
 #   public_key = file("~/.ssh/liorm-tf-key_rsa.pub")
 # }
-resource "aws_key_pair" "liorm-tf-key_rsa" {
-  key_name   = "liorm-tf-key_rsa"
-  public_key = var.public_key_content
-}
+# resource "aws_key_pair" "liorm-tf-key_rsa" {
+#   key_name   = "liorm-tf-key_rsa"
+#   public_key = var.public_key_content
+# }
 
 resource "aws_instance" "liorm-TED" {
   count = terraform.workspace == "default" ? 1 : 3
   ami                    = var.AMI
   instance_type          = var.TYPE
-  key_name               = aws_key_pair.liorm-tf-key_rsa.id
+  # key_name               = aws_key_pair.liorm-tf-key_rsa.id
   subnet_id = var.subnets_1a
   availability_zone      = "us-east-1a"
   vpc_security_group_ids = [var.security_group]
