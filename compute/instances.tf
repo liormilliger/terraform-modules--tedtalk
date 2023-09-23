cc114aad0e8eaa96897a5775242d9aa93ff101bc
liorm <lior@email.com>
2023-09-23 10:30:34 +0300

    IAM role and new userdata.sh for EC2

--- compute/instances.tf
+++ compute/instances.tf
@@ -6,12 +6,21 @@ resource "aws_key_pair" "liorm-tf-key_rsa" {
 #   key_name   = "liorm-ted-key"
 #   public_key = var.public_key_content
 # }
+data "aws_iam_role" "liorm-ec2-ecr" {
+  name = "liorm-ec2-ecr"  # Replace with the actual name of your IAM role
+}
+
+resource "aws_iam_instance_profile" "access_ecr" {
+  name = "liorm_EC2_to_ECR"
+  role = data.aws_iam_role.liorm-ec2-ecr
+}
 
 resource "aws_instance" "liorm-TED" {
   count = terraform.workspace == "default" ? 1 : 3
   ami                    = var.AMI
   instance_type          = var.TYPE
   key_name               = "liorm-tf-key_rsa"
+  iam_instance_profile = aws_iam_instance_profile.access_ecr.name
   subnet_id = var.subnets_1a
   availability_zone      = "us-east-1a"
   vpc_security_group_ids = [var.security_group]
--- compute/userdata.sh
+++ compute/userdata.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 # Install Docker and Docker Compose
-apt-get update -y
-apt-get install -y docker.io
+apt update -y
+apt install docker -y
+apt install -y docker.io
+apt install docker-compose
 # curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # chmod +x /usr/local/bin/docker-compose
 
 # Fetch docker-compose.yaml from S3 bucket and perform docker-compose up
-aws s3 cp s3://liorm-ted/userdata/docker-compose.yaml ./
+aws s3 cp s3://liorm-ted/userdata/docker-compose.yaml ./docker-compose.yaml
 # cd /path/to/destination/
-docker compose up --build
+# docker compose up --build
+
+# Authenticate Docker to the ECR registry (replace with your AWS region)
+$(aws ecr get-login --no-include-email --region YOUR_REGION)
+
+# Pull the Docker images from ECR
+docker pull 644435390668.dkr.ecr.us-east-1.amazonaws.com/liorm-ted:latest
+docker pull 644435390668.dkr.ecr.us-east-1.amazonaws.com/nginx-img:latest
+
+# Use Docker Compose to start the services defined in the copied docker-compose.yaml file
+docker-compose up -f ./docker-compose.yaml --build
