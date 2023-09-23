1408f42de25e26813819cd3034bfc742fc072a9f
liorm <lior@email.com>
2023-09-23 21:07:07 +0300

    still sorting out userdata.sh file

--- compute/userdata.sh
+++ compute/userdata.sh
@@ -9,7 +9,7 @@ apt install docker-compose -y
 apt install awscli -y
 apt update -y
 # curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+# chmod +x /usr/local/bin/docker-compose
 sleep 30
 # Fetch docker-compose.yaml from S3 bucket and perform docker-compose up
 aws s3 cp s3://liorm-ted/userdata/docker-compose.yaml ./docker-compose.yaml
