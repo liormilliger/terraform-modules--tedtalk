f121e327a07ba78d736a0caf2e158568aeab06e9
liorm <lior@email.com>
2023-09-24 15:59:14 +0300

    sorting files - deleting userdata and changing name to userdata1>userdata

--- compute/userdata.sh
+++ compute/userdata.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
 # Install services
-apt update 
-apt install docker -y
-apt install -y docker.io
-apt install docker-compose -y
-apt install awscli -y
+apt-get update && apt-get upgrade -y
+apt-get install docker -y
+apt-get install -y docker.io
+apt-get install docker-compose -y
 
+snap install aws-cli --classic # newest version
 usermod -aG docker ubuntu
 
+sudo -iu ubuntu << EOF
 #Copying files from s3
-aws s3 cp s3://liorm-ted/userdata/docker-compose.yaml /home/ubuntu/docker-compose.yaml
-aws s3 cp s3://liorm-ted/userdata/nginx.conf /home/ubuntu/nginx.conf
+aws s3 cp s3://liorm-ted/userdata/docker-compose.yaml ./docker-compose.yaml
+aws s3 cp s3://liorm-ted/userdata/nginx.conf ./nginx.conf
 
 # Fetching images 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 644435390668.dkr.ecr.us-east-1.amazonaws.com | docker pull 644435390668.dkr.ecr.us-east-1.amazonaws.com/liorm-ted:ted-search
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 644435390668.dkr.ecr.us-east-1.amazonaws.com | docker pull 644435390668.dkr.ecr.us-east-1.amazonaws.com/liorm-ted:nginx-img
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 644435390668.dkr.ecr.us-east-1.amazonaws.com 
+
+docker pull 644435390668.dkr.ecr.us-east-1.amazonaws.com/liorm-ted:ted-search
+docker pull 644435390668.dkr.ecr.us-east-1.amazonaws.com/liorm-ted:nginx-img
+
 docker pull ubuntu/memcached
 
-# # Use Docker Compose to start the services defined in the copied docker-compose.yaml file
-docker-compose up --build
+docker-compose up 
+EOF
+
