8f93f1006eec5ca44714cdb69830fb0b03ab92df
liorm <lior@email.com>
2023-09-24 10:55:47 +0300

    sorting userdata.sh after succeeding manual deployment

--- compute/userdata.sh
+++ compute/userdata.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
 
-# Install Docker and Docker Compose
+# Install services
 apt update -y
 apt upgrade -y
 apt install docker -y
 apt install -y docker.io
 apt install docker-compose -y
 apt install awscli -y
-apt update -y
-# curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# chmod +x /usr/local/bin/docker-compose
-sleep 30
-# Fetch docker-compose.yaml from S3 bucket and perform docker-compose up
+
+usermod -aG docker ubuntu
+
+#Copying files from s3
 aws s3 cp s3://liorm-ted/userdata/docker-compose.yaml ./docker-compose.yaml
 aws s3 cp s3://liorm-ted/userdata/nginx.conf ./nginx.conf
 
-
-
-
-# # Authenticate Docker to the ECR registry (replace with your AWS region)
-# aws ecr get-login --no-include-email --region us-east-1
-
-# # Pull the Docker images from ECR
-# docker pull 644435390668.dkr.ecr.us-east-1.amazonaws.com/liorm-ted:latest
-# docker pull 644435390668.dkr.ecr.us-east-1.amazonaws.com/nginx-img:latest
+# Fetching images 
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 644435390668.dkr.ecr.us-east-1.amazonaws.com | docker pull 644435390668.dkr.ecr.us-east-1.amazonaws.com/liorm-ted:ted-search
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 644435390668.dkr.ecr.us-east-1.amazonaws.com | docker pull 644435390668.dkr.ecr.us-east-1.amazonaws.com/liorm-ted:nginx-img
+docker pull ubuntu/memcached
 
 # # Use Docker Compose to start the services defined in the copied docker-compose.yaml file
 # docker-compose up --build
